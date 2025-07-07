91e9dcab2aa9821f2ba1f96ac606a21e72a1680f
thsaudgh8 <thsaudgh8@naver.com>
2025-07-07 12:41:25 +0900

    날씨 데이터 받아와서 카드 형식으로 제작/ 최저기온 표기오류

---
+++ my-react-app/src/hooks/useLocation.js
@@ -0,0 +1,31 @@
+
+import { useState, useEffect } from "react";
+
+export function useLocation() {
+  const [location, setLocation] = useState({ lat: null, lon: null });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      setLoading(false);
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+        setLoading(false);
+      },
+      (err) => {
+        setError(err.message);
+        setLoading(false);
+      }
+    );
+  }, []);
+
+  return { location, loading, error };
+}
--- my-react-app/src/services/weatherService.js
+++ my-react-app/src/services/weatherService.js
@@ -3,7 +3,7 @@
 // my-react-app/src/services/weatherService.js
 export async function fetchShortTermForecast({ baseDate, baseTime, nx, ny }) {
   const serviceKey = import.meta.env.VITE_WEATHER_API_KEY;;
-  const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${serviceKey}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
+  const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${serviceKey}&pageNo=1&numOfRows=100&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
 
   const res = await fetch(url);
   if (!res.ok) throw new Error("API 요청 실패");
