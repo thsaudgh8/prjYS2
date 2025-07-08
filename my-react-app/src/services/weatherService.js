1e6ead7e9c6c6b98ff64076fd4c87d71890b13cc
thsaudgh8 <thsaudgh8@naver.com>
2025-07-08 10:12:35 +0900

    주석처리로 이전코드 유지했던 부분 삭제

--- my-react-app/src/services/weatherService.js
+++ my-react-app/src/services/weatherService.js
@@ -1,17 +1,5 @@
 // // API 요청 관련 함수 사용
 
-// // my-react-app/src/services/weatherService.js
-// export async function fetchShortTermForecast({ baseDate, baseTime, nx, ny }) {
-//   const serviceKey = import.meta.env.VITE_WEATHER_API_KEY;;
-//   const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${serviceKey}&pageNo=1&numOfRows=100&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${nx}&ny=${ny}`;
-
-//   const res = await fetch(url);
-//   if (!res.ok) throw new Error("API 요청 실패");
-//   const data = await res.json();
-//   return data.response.body.items.item; // 예보 리스트 반환
-// }
-
-// services/weatherService.js
 const SERVICE_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 const BASE_URL = 'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
 
