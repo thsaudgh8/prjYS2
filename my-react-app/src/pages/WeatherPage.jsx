de76d67825bc9c5e2e8324bd344d6f6584916a89
thsaudgh8 <thsaudgh8@naver.com>
2025-07-09 14:53:40 +0900

    weather page ui 조정

--- my-react-app/src/pages/WeatherPage.jsx
+++ my-react-app/src/pages/WeatherPage.jsx
@@ -3,8 +3,36 @@ import WeatherCard from '../components/WeatherCard';
 
 const WeatherPage = () => {
   return (
-    <div style={{ padding: '1rem' }}>
-      <WeatherCard />
+    <div
+      style={{
+        display: 'flex',
+        paddingTop: '64px',   // 네비바 높이만큼 띄움
+        height: 'calc(100vh - 64px)', // 네비바 제외 화면 전체 높이
+        boxSizing: 'border-box',
+      }}
+    >
+      {/* 좌측 영역: WeatherCard */}
+      <div
+        style={{
+          width: '50%',       // 좌우 50%씩 분할 (필요시 조절)
+          padding: '1rem',
+          overflowY: 'auto',  // 내용 많으면 세로 스크롤 가능
+          borderRight: '1px solid #ccc',
+        }}
+      >
+        <WeatherCard />
+      </div>
+
+      {/* 우측 영역: 빈 영역 혹은 다른 컨텐츠 넣을 공간 */}
+      <div
+        style={{
+          width: '50%',
+          padding: '1rem',
+          overflowY: 'auto',
+        }}
+      >
+        {/* 필요시 여기에 우측 컨텐츠 */}
+      </div>
     </div>
   );
 };
