d25da13fa4633e20ddc2cfdf527269d09bc3416d
thsaudgh8 <thsaudgh8@naver.com>
2025-07-16 12:42:41 +0900

    test용 코드 삽입

--- my-react-app/src/pages/BusPage.jsx
+++ my-react-app/src/pages/BusPage.jsx
@@ -190,3 +190,22 @@ function BusInfo({ lat, lng }) {
 }
 
 export default Home;
+
+
+
+
+
+// // test용
+
+// import { Container, Typography } from '@mui/material';
+
+// function Home() {
+//   return (
+//     <Container>
+//       <Typography variant="h4" mt={4}>미세먼지 화면</Typography>
+//       <Typography variant="body1" mt={2}>여기에 오늘의 날씨와 미세먼지 정보가 표시됩니다.</Typography>
+//     </Container>
+//   );
+// }
+
+// export default Home;
